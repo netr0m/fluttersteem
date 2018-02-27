69e9ca249755484cd35f5217d0661207425a3b41
mortea15 <mortea15@student.uia.no>
2018-02-27 14:45:50 +0100

    Added revocation endpoint (logout)

--- lib/src/endpoints.dart
+++ lib/src/endpoints.dart
@@ -3,3 +3,6 @@ final Uri authEndpoint = Uri.parse('https://steemconnect.com/api/oauth2/authoriz
 
 /// The URL to access to exchange an authorization token for an access token.
 final Uri tokenEndpoint = Uri.parse('http://localhost:8000/complete/steemconnect/');
+
+/// The URL to access to revoke an authorization token for an application
+final Uri revocationEndpoint = Uri.parse('https://v2.steemconnect.com/api/oauth2/token/revoke');
