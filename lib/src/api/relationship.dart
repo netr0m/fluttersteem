9b55906fd9f0af8975a5f76ddf048a4d1880094d
mortea15 <mortea15@student.uia.no>
2018-02-28 18:48:06 +0100

    followers/following api

--- lib/src/api/relationship.dart
+++ lib/src/api/relationship.dart
@@ -5,10 +5,24 @@ import '../models/models.dart';
 /// https://v2.steemconnect.com/docs/steemjs#api/
 abstract class SteemRelationshipsApi {
   /// Get the list of users this user follows
-  Future<List<User>> getFollowing();
+  ///
+  /// follower - Name of the target
+  /// startFollowing - Used for pagination
+  /// followType - Usually 'blog'
+  /// limit - Limit the number of matches returned
+  ///
+  /// https://api.steemjs.com/get_following?follower=NAME&startFollowing=NULL&followType=TYPE&limit=LIMIT
+  Future<List<User>> getFollowing(String follower, int limit, {String startFollowing, String followType});
 
   /// Get the list of users this user is followed by
-  Future<List<User>> getFollowers();
+  ///
+  /// following - Name of the target
+  /// startFollower - Used for pagination
+  /// followType - Usually 'blog'
+  /// limit - Limit the number of matches returned
+  ///
+  /// https://api.steemjs.com/get_followers?following=NAME&startFollower=NULL&followType=TYPE&limit=LIMIT
+  Future<List<User>> getFollowers(String following, int limit, {String startFollower, String followType});
 
   Future<Relationship> modify(String action);
 }
