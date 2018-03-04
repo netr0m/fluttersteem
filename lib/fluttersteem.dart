235b54935871ac699078caa8b0f4137fdb9a794f
mortea15 <mortea15@student.uia.no>
2018-03-04 06:08:57 +0100

    fluttersteem lib

--- lib/fluttersteem.dart
+++ lib/fluttersteem.dart
@@ -1,3 +1,8 @@
+/// A cross-platform client library for the SteemConnect and SteemJS API's
 library fluttersteem;
 
-//export 'src/';
+export 'src/api/api.dart';
+export 'src/fluttersteem.dart';
+export 'src/models/impl_models.dart' show SteemException;
+export 'src/models/models.dart';
+export 'src/scopes.dart';
