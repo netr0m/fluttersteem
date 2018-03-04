4318a014e95f10852a1c74be0bd12efeebb354a8
mortea15 <mortea15@student.uia.no>
2018-03-04 05:59:52 +0100

    Added abstractions for extracting data

--- lib/src/api/reply.dart
+++ lib/src/api/reply.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+
 import '../models/models.dart';
 
 /// An abstraction to extract reply information from Steem
@@ -17,7 +18,8 @@ abstract class SteemCommentsApiPost {
   /// Create a comment
   /// https://steemconnect.com/sign/comment?parent_author=AUTHOR&parent_permlink=PARENT_PERMLINK&author=AUTHOR&permlink=REPLY_PERMLINK&title=TITLE&body=BODY&json_metadata=VALUE
   Future<bool> createComment(String parentAuthor, String parentPermlink,
-      String author, String permlink, String title, String body, {String jsonMetadata});
+      String author, String permlink, String title, String body,
+      {String jsonMetadata});
 
   /// Remove a comment either on the authenticated user's post object or authored by the authenticated user
   /// https://steemconnect.com/sign/delete_comment?author=value&permlink=value
--- lib/src/api/steem.dart
+++ lib/src/api/steem.dart
@@ -1,11 +1,10 @@
 import '../models/models.dart';
-import 'reply.dart';
-import 'user.dart';
 import 'post.dart';
-import 'vote.dart';
-import 'tag.dart';
 import 'relationship.dart';
-// TODO: Finish importing
+import 'reply.dart';
+import 'tag.dart';
+import 'user.dart';
+import 'vote.dart';
 
 /// An abstraction to extract information from Steem
 /// https://v2.steemconnect.com/docs/steemjs#api/
@@ -16,21 +15,21 @@ abstract class SteemApi {
   /// The user who is logged in for this session
   User get user;
 
-  /// An abstraction to extract comment data from Steem
-  SteemCommentsApi get replies;
-
   /// An abstraction to extract post data from Steem
   SteemPostsApi get posts;
 
-  /// An abstraction to extract upvote data from Steem
-  SteemVotesApi get votes;
-
   /// An abstraction to extract relationship data from Steem
   SteemRelationshipsApi get relationships;
+
+  /// An abstraction to extract comment data from Steem
+  SteemCommentsApi get replies;
 
   /// An abstraction to extract tag data Steem
   SteemTagsApi get tags;
 
   /// An abstraction to extract user data from Steem
   SteemUsersApi get users;
+
+  /// An abstraction to extract upvote data from Steem
+  SteemVotesApi get votes;
 }
