0baf18ea62daccb3848d098b4452b91ee29a5e94
mortea15 <mortea15@student.uia.no>
2018-03-04 06:03:09 +0100

    Minor fixes, imports added

--- lib/src/requestor.dart
+++ lib/src/requestor.dart
@@ -1,18 +1,22 @@
 import 'dart:async';
+
 import 'package:http/src/base_client.dart' as http;
 import 'package:http/src/request.dart' as http;
 import 'package:http/src/response.dart' as http;
+
 import 'models/impl_models.dart';
 
 abstract class Requestor {
-  Uri buildUri(String path, {Map<String, String> queryParameters, String method});
+  Uri buildUri(String path,
+      {Map<String, String> queryParameters, String method});
 
   Map<String, String> buildBody(Map<String, String> body);
 
-  Future<SteemResponse> request(String path, {Map<String, String> body, Map<String, String> queryParameters, String method}) {
+  Future<SteemResponse> request(String path, {Map<String, String> body, Map<
+      String,
+      String> queryParameters, String method}) {
     var uri = buildUri(path, queryParameters: queryParameters, method: method);
     var req = new http.Request(method ?? 'GET', uri);
-    req.headers['Authorization'] = me.accessToken;
     req.headers['Content-Type'] = 'application/json';
     req.headers['Accept'] = 'application/json';
 
